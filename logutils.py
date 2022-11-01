a1e0d12c92d741408c788ba047b5e0b5b5bc5bb5
Ping-yu Chou <mail@pcchou.me>
2022-11-01 21:19:41 +0800

    Fix RefreshError after a while

--- logutils.py
+++ logutils.py
@@ -4,6 +4,7 @@ import os.path
 from config import *
 
 from google.auth.transport.requests import Request
+from google.auth.exceptions import RefreshError
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
@@ -20,7 +21,14 @@ if os.path.exists('token.json'):
 # If there are no (valid) credentials available, let the user log in.
 if not creds or not creds.valid:
     if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
+        try:
+            creds.refresh(Request())
+        except RefreshError:
+            if os.path.exists('token.json'):
+                os.remove('token.json')
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
     else:
         flow = InstalledAppFlow.from_client_secrets_file(
             'credentials.json', SCOPES)
